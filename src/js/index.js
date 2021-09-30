45ed18f2c74fa4adebd680582951bed7c006b348
Elsa Brown <elsambrown@gmail.com>
2021-09-30 13:05:07 -0400

    waits for document ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,25 @@
 import '../scss/style.scss';
 
-const init = () => {
-    console.log('HELLO SCRIPT BUNDLER');
-
+const addDiv = () => {
     const newDiv = document.createElement('div');
     newDiv.className = 'script-bundler-div';
     newDiv.innerHTML = "This element was created with script-bundler";
 
     document.body.appendChild(newDiv);
+}
+
+const init = () => {
+    console.log('HELLO SCRIPT BUNDLER');
+    
+    if (document.readyState !== "loading") {
+        addDiv();
+    } else {
+        document.onreadystatechange = () => {
+            if ( document.readyState !== 'loading') {
+                addDiv();
+            }
+        }
+    }
 }
 
 init();
